3a5618719bc5b32e4ab9f7ed3d0ead793da08d88
Mitchell Rodríguez <mitrodle@gmail.com>
2017-06-01 12:41:56 -0500

    Refactorizado

--- flickr-fetcher.js
+++ flickr-fetcher.js
@@ -2,8 +2,12 @@ var FlickrFetcher;
 
 FlickrFetcher = {
     photoObjToURL: function(photoObj) {
-        return 'https://farm' + photoObj.farm + '.staticflickr.com/' + photoObj.server + '/' + photoObj.id + '_' +
-            photoObj.secret + '_b.jpg';
+        return [ 'https://farm',
+            photoObj.farm, '.staticflickr.com/', 
+            photoObj.server, '/',
+            photoObj.id, '_',
+            photoObj.secret, '_b.jpg'
+        ].join('');
     }
 };
 
