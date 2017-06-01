c2431eb2939c12dd202b2d185110327c8970481e
Mitchell Rodríguez <mitrodle@gmail.com>
2017-06-01 12:51:32 -0500

    Cuarto avance

--- flickr-fetcher.js
+++ flickr-fetcher.js
@@ -8,7 +8,15 @@ FlickrFetcher = {
             photoObj.id, '_',
             photoObj.secret, '_b.jpg'
         ].join('');
+    },
+
+    transformPhotoObj: function() {
+        return {
+            title: 'Dog goes to desperate measure to avoid walking on a leash',
+            url:   'https://farm2.staticflickr.com/1669/25373736106_146731fcb7_b.jpg'
+        };
     }
+
 };
 
 module.exports = FlickrFetcher;
