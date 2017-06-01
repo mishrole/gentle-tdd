9041af10bd2df023dab4356707acbd473f7f5d05
Mitchell Rodríguez <mitrodle@gmail.com>
2017-06-01 12:40:52 -0500

    Tercer avance

--- flickr-fetcher.js
+++ flickr-fetcher.js
@@ -1,8 +1,10 @@
 var FlickrFetcher;
 
 FlickrFetcher = {
-    photoObjToURL: function() {
-        return 'https://farm2.staticflickr.com/1577/24770505034_31a9986429_b.jpg';
+    photoObjToURL: function(photoObj) {
+        return 'https://farm' + photoObj.farm + '.staticflickr.com/' + photoObj.server + '/' + photoObj.id + '_' +
+            photoObj.secret + '_b.jpg';
     }
 };
+
 module.exports = FlickrFetcher;
